1adbee1ae43cc76213d7aa4c9d1ba6cc504dac2a
flekst <flekst@6b09c9dc-fc6c-11de-ae41-5b335d110c9e>
2010-01-08 22:33:08 +0000

    MftFinder can find mft Mirror position MftRecord get simple test for matching mft record sturcture
    added common function testfor(needle, needleLen, buffer,bufferLen) 
    	returnval is offset of needle in buffer or -1

--- trunk/src/common.h
+++ trunk/src/common.h
@@ -7,8 +7,13 @@
 #include <QObject>
 #include <QDebug>
 #include <QString>
+#include <QApplication>
+#include <QIODevice>
+
+
 #define DBG qDebug() <<  __FILE__ << __LINE__ <<__FUNCTION__ <<":"
 
-// place your code here
 
+ int testFor (char *data, int data_len, char *buffer, int buffer_len);
+ 
 #endif // __COMMON_H__
---
+++ trunk/src/mftrecord.h
@@ -0,0 +1,23 @@
+#ifndef MFTRECORD_H
+#define MFTRECORD_H
+#include "common.h"
+//
+
+#define MFT_RECORD_LEN 1024
+//
+class mftrecord  
+{
+protected:
+	char *data;
+
+public:
+	void update();
+	void dump();
+	void setData(char *value) {	data = value;}
+	char *getData() { return data;	}
+	int isPossibleValid();
+	
+	mftrecord();
+	
+};
+#endif

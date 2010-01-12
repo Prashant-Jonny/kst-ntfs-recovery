bad9133ce29eb94dd43af99ae5ef65355ce5967f
flekst <flekst@6b09c9dc-fc6c-11de-ae41-5b335d110c9e>
2010-01-12 23:00:35 +0000

    Fixed keywords

--- update_keywords.sh
+++ update_keywords.sh
@@ -2,7 +2,7 @@
 cd src
 SOURCE_KEYWORDS="Author Date  Rev HeadURL"
 for i in *.{cpp,h} ; do
-    echo svn propset svn:keywords ${SOURCE_KEYWORDS} $i
+    svn propset svn:keywords "${SOURCE_KEYWORDS}" $i
 done
 cd ..
 
